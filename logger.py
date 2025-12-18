6e9cff92eb7f7267f3724ae7b941bb49796db83a
Sahil Singh <sahilsingh8300@gmail.com>
2025-12-18 06:19:03 +0530

    Add logging utility module

---
+++ logger.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+from typing import Optional
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+def get_logger(name: Optional[str] = None) -> logging.Logger:
+    if name is None:
+        name = 'rag_system'
+    return setup_logger(name)
